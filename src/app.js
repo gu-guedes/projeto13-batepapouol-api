f69bba8f9c1a5c52a957328d9e655de4f2de6c6c
gu-guedes <guguedes2020@gmail.com>
2023-04-13 17:37:20 -0300

    requisitos post e get /participants

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv"
+import dayjs from "dayjs"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 dotenv.config()
+console.log(dayjs().format('HH:mm:ss'))
+
 
 let db
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
@@ -15,11 +18,49 @@ mongoClient.connect()
 .catch((err) => console.log(err.message))
 
 
-//app.get("/participants", (req, res) => {
-//   db.collection("participante").find().toArray()
-//  .then(participante => res.send(participante))
-//.catch(err => res.status(500).send(err.message))
-//})
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+    try {
+        if(name === ""){
+            return res.sendStatus(422)
+        }
+        const nomeExistente = await db.collection("participants").findOne({name})
+        if(nomeExistente){
+            return res.sendStatus(409)
+        }
+        
+        db.collection("participants").insertOne({name, lastStatus: Date.now()})
+        
+        
+        db.collection("messages").insertOne({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss') })
+
+        return res.sendStatus(201)
+        
+    } catch(error){
+        return res.status(500).send(error)
+
+    }
+    
+})
+
+app.get("/participants", (req, res) => {
+  db.collection("participants").find().toArray()
+ .then(participante => res.send(participante))
+.catch(err => res.status(500).send(err.message))
+})
+
+app.post("/messages", (req, res) => {
+    const {to, text, type} =  req.body
+    
+        if(to || text === ""){
+            return res.sendStatus(422)
+        }
+        db.collection("messages").insertOne({to, text, type})
+        return res.sendStatus(201)
+        
+        
+    
+})
 
 
 const PORT = 5000
