be4abc396048317d3a2ae8f95b3cc1546ab3af1b
gu-guedes <guguedes2020@gmail.com>
2023-04-14 17:41:01 -0300

    requisitos: post/messages, get/messages, post/status (sem joi)

--- src/app.js
+++ src/app.js
@@ -21,10 +21,12 @@ mongoClient.connect()
 app.post("/participants", async (req, res) => {
     const { name } = req.body
     try {
-        if(name === ""){
+        //tem que conter um "name" no body 
+        if(!name){
             return res.sendStatus(422)
         }
         const nomeExistente = await db.collection("participants").findOne({name})
+        
         if(nomeExistente){
             return res.sendStatus(409)
         }
@@ -44,22 +46,85 @@ app.post("/participants", async (req, res) => {
 })
 
 app.get("/participants", (req, res) => {
+
   db.collection("participants").find().toArray()
  .then(participante => res.send(participante))
 .catch(err => res.status(500).send(err.message))
 })
 
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
+     const user  =  req.headers.user
+     
+    
     const {to, text, type} =  req.body
     
-        if(to || text === ""){
+        if(!to || !text){
             return res.sendStatus(422)
         }
-        db.collection("messages").insertOne({to, text, type})
+        
+        if(type !== "private_message" && type !== "message"){
+            return res.sendStatus(422)
+
+        }
+         const participanteExistente = await db.collection("participants").findOne({name: user})
+        
+    
+        if(!participanteExistente){
+             return res.sendStatus(422)
+         }   
+         if(!user){
+            return res.sendStatus(422)
+         }   
+       
+        db.collection("messages").insertOne({from: user, to, text, type, time: dayjs().format('HH:mm:ss')})
         return res.sendStatus(201)
         
         
     
+})
+app.get("/messages", async (req, res) => {
+    const user  =  req.headers.user
+    
+    const { limit } = req.query
+    const mensagens = await db.collection("messages").find().toArray()
+    
+    if (isNaN(Number(limit))) {
+        return res.sendStatus(422)
+    }
+   
+    if(limit === "0" || limit < "0"){
+        return res.sendStatus(422)
+    }
+    if(limit){
+        const mensagensFiltradas = mensagens.slice(-limit)
+        return res.send(mensagensFiltradas)
+    }
+    const mensagensTodos = await db.collection("messages").find({$or: [{to: "Todos"}, {to: user}, {from: user}]}).toArray()
+    .then(mensagensTodos=> res.send(mensagensTodos))
+    .catch(err => res.status(500).send(err.message))
+
+    
+    
+})
+app.post("/status", async (req, res) => {
+    const user = req.headers.user
+    console.log(user)
+
+    if(!user){
+        return res.sendStatus(404)
+    }
+    const participanteExistente = await db.collection("participants").findOne({name: user})
+         
+
+     if(!participanteExistente){
+        return res.sendStatus(404)
+    }
+
+    db.collection("participants").updateOne({name: user}, {$set:{lastStatus: Date.now()}})
+
+    res.sendStatus(200)
+
+
 })
 
 
